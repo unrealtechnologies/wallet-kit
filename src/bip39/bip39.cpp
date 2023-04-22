8ee07699f15fea155ab7ad2648c4f1a0ae7a485e
Ariel Saldana <ariel@unreal.io>
2023-04-22 09:29:51 -0400

    add validation for seed phrase (WIP)

--- src/bip39/bip39.cpp
+++ src/bip39/bip39.cpp
@@ -5,6 +5,7 @@
 #include <utils.h>
 #include <wallet-kit/bip39.h>
 #include <wallet-kit/bip39/english_wordlist.h>
+#include "wallet-kit/cryptography/crypto_utils.h"
 #include <botan/hash.h>
 #include <botan/pbkdf2.h>
 #include <botan/mac.h>
@@ -30,23 +31,22 @@ std::string Bip39::entropyToMnemonic(std::vector<uint8_t> &entropy) {
     return fullEntropyBitsToMnemonicWords(fullEntropyBinaryString);
 }
 
-std::vector<std::uint8_t> getBytes(std::string const &s)
-{
+std::vector<std::uint8_t> getBytes(std::string const &s) {
     std::vector<std::uint8_t> bytes;
     bytes.reserve(s.size());
 
-    std::transform(std::begin(s), std::end(s), std::back_inserter(bytes), [](char c){
+    std::transform(std::begin(s), std::end(s), std::back_inserter(bytes), [](char c) {
         return std::uint8_t(c);
     });
 
     return bytes;
 }
 
-std::vector<uint8_t> Bip39::mnemonicToSeed(const std::string& mnemonic) {
+std::vector<uint8_t> Bip39::mnemonicToSeed(const std::string &mnemonic) {
     return mnemonicToSeed(mnemonic, "");
 }
 
-std::vector<uint8_t> Bip39::mnemonicToSeed(std::string mnemonic, const std::string& passphrase) {
+std::vector<uint8_t> Bip39::mnemonicToSeed(std::string mnemonic, const std::string &passphrase) {
     // Define the password and salt to use
     std::string salt = "mnemonic";
     salt += passphrase;
@@ -105,38 +105,52 @@ std::vector<uint8_t> Bip39::getEntropyChecksum(std::vector<uint8_t> &entropy) {
 }
 
 bool Bip39::validateMnemonic(const std::string &mnemonic) {
-    auto delimiter = " ";
-    auto wordVector = WalletKitUtils::split(mnemonic, delimiter);
+    auto words = WalletKitUtils::split(mnemonic, " ");
+    auto wordsIndexes = seedStringToWordIndexVector(words);
 
-    if (wordVector.size() > 24 || wordVector.size() < 12 || wordVector.size() % 2 != 0) {
+    if (words.size() != wordsIndexes.size()) {
         return false;
     }
 
-    // if the entropy is 128 bits aka 16 bytes, how do we turn that into something we can verify.
-    // the checksum length is the remainder of the length divided by 8. if 0 = 8
-    const auto fullEntropyLength = wordVector.size() * numberOfBitsPerWord;
-    const auto checksumLength = fullEntropyLength % 8 == 0 ? 8: fullEntropyLength % 8;
-
-    std::cout << checksumLength << std::endl;
-    std::cout << fullEntropyLength << std::endl;
-
-
-    std::ostringstream oss;
-    for (const auto &word: wordVector) {
-        auto binaryStr = WalletKitUtils::strToBinaryString(word);
-        oss << binaryStr.substr(0, numberOfBitsPerWord);
+    if (words.size() > 24 || words.size() < 12 || words.size() % 4 != 0) {
+        return false;
     }
 
-    const auto finalChecksumBinaryString = oss.str();
-    const auto entropyBinaryString = finalChecksumBinaryString.substr(0, finalChecksumBinaryString.size() - checksumLength);
-    std::vector<uint8_t> entropyBinary(entropyBinaryString.begin(), entropyBinaryString.end());
-    const auto generatedChecksum = entropyToMnemonic(entropyBinary);
+    // calculate the checksum length
+    const auto fullEntropyLength = words.size() * numberOfBitsPerWord;
+    const auto checksumLength = fullEntropyLength % 8 == 0 ? 8 : fullEntropyLength % 8;
+
+    // turn the indexes back to 11 bits and appends it to a string
+    std::ostringstream oss;
+    for (const auto &index: wordsIndexes) {
+        oss << WalletKitUtils::uint16ToBinary(index, numberOfBitsPerWord);
+    }
+    const auto bits = oss.str();
+    const auto entropyBits = bits.substr(0, bits.length() - checksumLength);
+    const auto checksumBits = bits.substr(bits.length() - 4, bits.length());
+
+    // reconstruct to bytes
+    auto bitsByteStorageVector = WalletKitUtils::split(entropyBits, 8);
+    auto entropyBytes = std::vector<uint8_t>();
+    for (std::string const &byteAsBits: bitsByteStorageVector) {
+        std::bitset<8> bit(byteAsBits);
+        auto byte = static_cast<unsigned char>(bit.to_ulong());
+        entropyBytes.push_back(byte);
+    }
+
+    // byte format
+    auto digest = WalletKitCryptoUtils::sha256(entropyBytes);
+    auto digestChecksum = WalletKitUtils::vecToBinaryString(digest).substr(0, checksumLength);
+
+    uint8_t checksum = digest[0];
+    std::vector<uint8_t> chec = {checksum};
 
 
-    std::cout << generatedChecksum << std::endl;
-    std::cout << finalChecksumBinaryString << std::endl;
-    std::cout << finalChecksumBinaryString.size() << std::endl;
-    return true;
+    if (digestChecksum == checksumBits) {
+        return true;
+    }
+
+    return false;
 }
 
 std::vector<uint16_t> Bip39::seedStringToWordIndexVector(const std::vector<std::string> &words) {
