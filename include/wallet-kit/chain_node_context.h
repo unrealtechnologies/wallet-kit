1be02125d33787ec9ccbf8a58ddb26c299591538
Ariel Saldana <ariel@unreal.io>
2023-04-26 09:44:37 -0400

    Added header to header files

--- include/wallet-kit/chain_node_context.h
+++ include/wallet-kit/chain_node_context.h
@@ -1,6 +1,24 @@
-//
-// Created by Ariel Saldana on 3/30/23.
-//
+/*
+ * This source code is the property of Unreal Technologies.
+ *
+ * Copyright (c) [2023] Unreal Technologies.
+ * All rights reserved.
+ *
+ * Author: Ariel Saldana https://github.com/ArielSaldana
+ *
+ * This source code is licensed under the Apache License Version 2.0 license found in the
+ * LICENSE file in the root directory of this source tree. By downloading,
+ * copying, installing or using this source code, you agree to the terms and
+ * conditions of the license.
+ *
+ * THIS SOURCE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOURCE CODE OR THE USE OR OTHER
+ * DEALINGS IN THE SOURCE CODE.
+ */
 
 #ifndef WALLET_KIT_LIB_CHAIN_NODE_CONTEXT_H
 #define WALLET_KIT_LIB_CHAIN_NODE_CONTEXT_H
